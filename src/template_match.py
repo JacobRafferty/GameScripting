2a22f4de88d04f776910d8b9f7328a954243d2c1
Jacob <jacobrafferty27@hotmail.com>
2024-11-18 21:06:03 -0600

    WIP - starting cleanup into main

--- src/template_match.py
+++ src/template_match.py
@@ -4,55 +4,72 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pyautogui
 import screeninfo
+import argparse
 
-# template = "img\\5gem.png"
-template = cv.imread('C:\\Users\\jacob\\Documents\Projects\\TheTowerAutomation\\img\\5g_closer.png', cv.IMREAD_GRAYSCALE)
-# template2 = template.copy()
-
-# width and height of the template to be matched
-w, h = template.shape[::-1]
-
-# take a sc of the screen ( try to replace this with an app capture)
-with mss() as sct:
-    screen = sct.shot(mon=2, output='screen.png')
-    print(f"sc name: {screen}")
-
-screen = cv.imread('screen.png', cv.IMREAD_GRAYSCALE)
-
-template_method = 'TM_CCOEFF'
-
-method = getattr(cv, template_method)
-
-res = cv.matchTemplate(screen, template, method)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-
-top_left = max_loc
-print(f'top left: {top_left}')
-
-bottom_right = (top_left[0] + w, top_left[1] + h)
-
-print(f"bottom right: {bottom_right}")
+def click_location(monitor_num, location):
+    monitors = screeninfo.get_monitors()
+    tower_screen = monitors[monitor_num]
+    x_abs = tower_screen.x + location[0]
+    y_abs = tower_screen.y + location[1]
+    pyautogui.FAILSAFE = False
+    pyautogui.click(x_abs, y_abs)
+    print(f"Clicked at ({x_abs}, {y_abs}) on Monitor 2.")
 
 
 
-cv.rectangle(screen,top_left, bottom_right, 255, 2)
+def get_screen(monitor):
+    with mss() as sct:
+        screen = sct.shot(mon=monitor, output='screen.png')
+        print(f"sc name: {screen}")
+    screen = cv.imread('screen.png', cv.IMREAD_GRAYSCALE)
+    return screen
 
-plt.subplot(121),plt.imshow(res,cmap = 'gray')
-plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(screen,cmap = 'gray')
-plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-plt.suptitle(template_method)
 
+
+
+def main():
+    parser = argparse.ArgumentParser(description="The Tower Automated")
+    parser.add_argument("-m", '--monitor', default=1)
+
+    args = parser.parse_args()
+
+    # get your screen
+    screen = get_screen(args.monitor)
+    matching_method = getattr(cv, 'TM_CCOEFF')
+
+    # grab template for gems
+    gems5_template = cv.imread('C:\\Users\\jacob\\Documents\Projects\\TheTowerAutomation\\img\\5g_closer.png', cv.IMREAD_GRAYSCALE)
+    # w, h = gems5_template.shape[::-1]
+
+    # grab template for restart
+
+    # LOOP
+
+    # grab sc
+        # see if we can return false on a failed template match
+
+    # match the template
+    matched_gems = cv.matchTemplate(screen, gems5_template, matching_method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(matched_gems)
+
+    # click the 5g 
+    click_location(args.monitor, max_loc)
+
+    # if you get a restart match, click it
+ 
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# cv.rectangle(screen,top_left, bottom_right, 255, 2)
+
+# plt.subplot(121),plt.imshow(res,cmap = 'gray')
+# plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(screen,cmap = 'gray')
+# plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+# plt.suptitle(template_method)
 # plt.show()
 
-
-monitors = screeninfo.get_monitors()
-
-tower_screen = monitors[1]
-
-x_abs = tower_screen.x + top_left[0]
-y_abs = tower_screen.y + top_left[1]
-
-pyautogui.click(x_abs, y_abs)
-
-print(f"Clicked at ({x_abs}, {y_abs}) on Monitor 2.")
