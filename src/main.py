1554bde6250ef707640a2f175d5d819b45e8f654
Jacob <jacobrafferty27@hotmail.com>
2024-11-22 19:43:41 -0600

    attempt to buy health and recover from restart

--- src/main.py
+++ src/main.py
@@ -31,8 +31,9 @@ def match_template(template_path, confidence_threshold=0.7):
             center_y = max_loc[1] + template_height // 2
 
             pyautogui.click(center_x, center_y)
+            return True
         else:
-            pass
+            return False
 
     except Exception as e:
         print(f"Error: {e}")
@@ -41,13 +42,35 @@ def match_template(template_path, confidence_threshold=0.7):
 if __name__ == "__main__":
     template_5g = os.getcwd() + "\\jacob_img\\\\5g_claim.png"
     floating_template_d = os.getcwd() + "\\jacob_img\\floating_down.png"
-    floating_template_l = os.getcwd() + "\\jacob_img\\\\floating_left.png"
-    floating_template_r = os.getcwd() + "\\jacob_img\\\\floating_right.png"
-    retry_template = os.getcwd() + "\\jacob_img\\\\retry_bigger.png"
+    floating_template_l = os.getcwd() + "\\jacob_img\\floating_left.png"
+    floating_template_r = os.getcwd() + "\\jacob_img\\floating_right.png"
+    retry_template = os.getcwd() + "\\jacob_img\\retry_bigger.png"
+    defense_stats_template = os.getcwd() + "\\jacob_img\\defense_stats.png"
+    health_template = os.getcwd() + "\\jacob_img\\health.png"
+
+    templates = [
+        # "\\jacob_img\\\\retry_bigger.png",
+        "\\jacob_img\\floating_down.png",
+        "\\jacob_img\\floating_left.png",
+        "\\jacob_img\\floating_right.png",
+        "\\jacob_img\\5g_claim.png",
+        "\\jacob_img\\health.png"
+    ]
 
     while True:    
-        match_template(template_5g)
-        match_template(floating_template_d)
-        match_template(floating_template_l)
-        match_template(floating_template_r)
-        match_template(retry_template)
+        for template_path in templates:
+            match_template(template_path)
+        
+        retry_clicked = match_template(retry_template)
+
+        if retry_clicked:
+            once = False
+            time.sleep(1)
+            while once is False
+                once = match_template(defense_stats_template)
+
+        time.sleep(0.1)
+        # match_template(template_5g)
+        # match_template(floating_template_d)
+        # match_template(floating_template_l)
+        # match_template(floating_template_r)
